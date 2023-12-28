9c13cba2ebed1613607458fee2b84b87cbd1f52d
gleb <gleb22ka@gmail.com>
2023-12-29 00:01:32 +0300

    Hw12 ready

---
+++ src/main/java/Hw12/Main.java
@@ -0,0 +1,21 @@
+package Hw12;
+
+import javax.lang.model.element.Name;
+
+public class Main {
+    public static void main(String[] args) {
+        Plate plate = new Plate(20);
+        Cat[] cats = new Cat[3];
+        cats[0] = new Cat("Барсик", 5);
+        cats[1] = new Cat("Мурзик", 10);
+        cats[2] = new Cat("Том", 15);
+
+        for (Cat cat : cats) {
+            cat.eat(plate);
+        }
+
+        for (Cat cat : cats) {
+            System.out.println(cat.name + " " + (cat.isHungry() ? "голоден" : "не голоден"));
+        }
+    }
+}
