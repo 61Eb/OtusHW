bf3d46f3f6bd4a915614ee1958ea304a41c56989
gleb <gleb22ka@gmail.com>
2024-02-13 23:15:18 +0300

    Hw 30 Demo

---
+++ src/main/java/hw/Homework30/ArrayCheckerTest.java
@@ -0,0 +1,15 @@
+package hw.Homework30;
+
+public class ArrayCheckerTest {
+    public static void main(String[] args) {
+        int[] array1 = {1, 2};
+        int[] array2 = {1, 1};
+        int[] array3 = {1, 3};
+        int[] array4 = {1, 2, 2, 1};
+
+        System.out.println(ArrayChecker.checkArray(array1));
+        System.out.println(ArrayChecker.checkArray(array2));
+        System.out.println(ArrayChecker.checkArray(array3));
+        System.out.println(ArrayChecker.checkArray(array4));
+    }
+}
